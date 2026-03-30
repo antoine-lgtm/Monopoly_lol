d1dae2537f13114482d8210c7a9ba2560b0725d6
anto <antoine.pelle0806@gmail.com>
2026-03-30 19:28:31 +0200

    lol theme added

--- newyorkcityedition.js
+++ newyorkcityedition.js
@@ -70,81 +70,109 @@ function luxurytax() {
 
 var square = [];
 
-square[0] = new Square("GO", "COLLECT $200 SALARY AS YOU PASS.", "white");
-square[1] = new Square("Port Authority Bus Terminal", "$60", "#4B0082", 60, 3, 2, 10, 30, 90, 160, 250);
-square[2] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "white");
-square[3] = new Square("Lincoln Tunnel", "$60", "#4B0082", 60, 3, 4, 20, 60, 180, 320, 450);
-square[4] = new Square("City Tax", "PAY 10% OR $200", "white");
-square[5] = new Square("LOMTO", "$200", "white", 200, 1);
-square[6] = new Square("Statue of Liberty", "$100", "#AACCFF", 100, 4, 6, 30, 90, 270, 400, 550);
-square[7] = new Square("Chance", "NEW YORK LOTTERY GAMES", "white");
-square[8] = new Square("Empire State Building", "$100", "#AACCFF", 100, 4, 6, 30, 90, 270, 400, 550);
-square[9] = new Square("Central Park", "$120", "#AACCFF", 120, 4, 8, 40, 100, 300, 450, 600);
-square[10] = new Square("Just Visiting", "", "white");
-square[11] = new Square("98.7 Kiss FM", "$140", "purple", 140, 5, 10, 50, 150, 450, 625, 750);
-square[12] = new Square("Con Edison Electric", "$150", "white", 150, 2);
-square[13] = new Square("Thirteen WNET", "$140", "purple", 140, 5, 10, 50, 150, 450, 625, 750);
-square[14] = new Square("The New York Times", "$160", "purple", 160, 5, 12, 60, 180, 500, 700, 900);
-square[15] = new Square("New York City Transit", "$200", "white", 200, 1);
-square[16] = new Square("New York Rangers", "$180", "orange", 180, 6, 14, 70, 200, 550, 750, 950);
-square[17] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "white");
-square[18] = new Square("New York Knicks", "$180", "orange", 180, 6, 14, 70, 200, 550, 750, 950);
-square[19] = new Square("Madison Square Garden", "$200", "orange", 200, 6, 16, 80, 220, 600, 800, 1000);
-square[20] = new Square("Free Parking", "", "white");
-square[21] = new Square("macy*s", "$220", "red", 220, 7, 18, 90, 250, 700, 875, 1050);
-square[22] = new Square("Chance", "NEW YORK LOTTERY GAMES", "white");
-square[23] = new Square("FAO Schwarz", "$220", "red", 220, 7, 18, 90, 250, 700, 875, 1050);
-square[24] = new Square("bloomingdale's", "$240", "red", 240, 7, 20, 100, 300, 750, 925, 1100);
-square[25] = new Square("Metro-North Railroad", "$200", "white", 200, 1);
-square[26] = new Square("Deloitte & Touche LLP", "$260", "yellow", 260, 8, 22, 110, 330, 800, 975, 1150);
-square[27] = new Square("SmithBarney", "$260", "yellow", 260, 8, 22, 110, 330, 800, 975, 1150);
-square[28] = new Square("Con Edison Gas", "$150", "white", 150, 2);
-square[29] = new Square("CITIBANK", "$280", "yellow", 280, 8, 24, 120, 360, 850, 1025, 1200);
-square[30] = new Square("Go to Jail", "Go directly to Jail. Do not pass GO. Do not collect $200.", "white");
-square[31] = new Square("The Regency Hotel", "$300", "green", 300, 9, 26, 130, 390, 900, 1100, 1275);
-square[32] = new Square("Essex House", "$300", "green", 300, 9, 26, 130, 390, 900, 1100, 1275);
-square[33] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "white");
-square[34] = new Square("The Plaza", "$320", "green", 320, 9, 28, 150, 450, 1000, 1200, 1400);
-square[35] = new Square("United Airlines", "$200", "white", 200, 1);
-square[36] = new Square("Chance", "NEW YORK LOTTERY GAMES", "white");
-square[37] = new Square("Tiffany & CO.", "$350", "blue", 350, 10, 35, 175, 500, 1100, 1300, 1500);
-square[38] = new Square("LUXURY TAX", "Pay $75", "white");
-square[39] = new Square("TRUMP TOWER", "$400", "blue", 400, 10, 50, 200, 600, 1400, 1700, 2000);
+square[0] = new Square("Fontaine", "RECEVEZ 200 GOLD EN PASSANT ICI.", "white");
+
+// GROUPE MARRON (Sivir & Skarner)
+square[1] = new Square("Sivir", "60 Gold", "#955438", 60, 3, 2, 10, 30, 90, 160, 250);
+square[2] = new Square("Coffre Hextech", "BUTIN EN COURS D'OUVERTURE...", "white");
+square[3] = new Square("Skarner", "60 Gold", "#955438", 60, 3, 4, 20, 60, 180, 320, 450);
+
+square[4] = new Square("Taxe de Sbires", "PAYEZ 10% OU 200 GOLD", "white");
+
+// GARE 1
+square[5] = new Square("Dragon des Océans", "200 Gold", "white", 200, 1);
+
+// GROUPE GRIS - FRELJORD (Lissandra, Volibear, Ornn)
+square[6] = new Square("Lissandra", "100 Gold", "#AACCFF", 100, 4, 6, 30, 90, 270, 400, 550);
+square[7] = new Square("Ping SS", "ENNEMI DISPARU ! SURPRISE...", "white");
+square[8] = new Square("Volibear", "100 Gold", "#AACCFF", 100, 4, 6, 30, 90, 270, 400, 550);
+square[9] = new Square("Ornn", "120 Gold", "#AACCFF", 120, 4, 8, 40, 100, 300, 450, 600);
+
+square[10] = new Square("Juste en visite", "", "white");
+
+// GROUPE ROSE - IONIA (Shen, Karma, Irelia)
+square[11] = new Square("Shen", "140 Gold", "purple", 140, 5, 10, 50, 150, 450, 625, 750);
+square[12] = new Square("Potion de HP", "150 Gold", "white", 150, 2);
+square[13] = new Square("Karma", "140 Gold", "purple", 140, 5, 10, 50, 150, 450, 625, 750);
+square[14] = new Square("Irelia", "160 Gold", "purple", 160, 5, 12, 60, 180, 500, 700, 900);
+
+// GARE 2
+square[15] = new Square("Dragon de Montagne", "200 Gold", "white", 200, 1);
+
+// GROUPE ORANGE - SHURIMA (Azir, Renekton, Nasus)
+square[16] = new Square("Azir", "180 Gold", "orange", 180, 6, 14, 70, 200, 550, 750, 950);
+square[17] = new Square("Coffre Hextech", "BUTIN EN COURS D'OUVERTURE...", "white");
+square[18] = new Square("Renekton", "180 Gold", "orange", 180, 6, 14, 70, 200, 550, 750, 950);
+square[19] = new Square("Nasus", "200 Gold", "orange", 200, 6, 16, 80, 220, 600, 800, 1000);
+
+square[20] = new Square("Pause Café (Scuttle)", "", "white");
+
+// GROUPE ROUGE - NOXUS (Sion, Kled, Swain)
+square[21] = new Square("Sion", "220 Gold", "red", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[22] = new Square("Ping SS", "ENNEMI DISPARU ! SURPRISE...", "white");
+square[23] = new Square("Kled", "220 Gold", "red", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[24] = new Square("Swain", "240 Gold", "red", 240, 7, 20, 100, 300, 750, 925, 1100);
+
+// GARE 3
+square[25] = new Square("Dragon Infernal", "200 Gold", "white", 200, 1);
+
+// GROUPE JAUNE - TARGON (Taric, Leona, Diana)
+square[26] = new Square("Taric", "260 Gold", "yellow", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[27] = new Square("Leona", "260 Gold", "yellow", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[28] = new Square("Potion de Mana", "150 Gold", "white", 150, 2);
+square[29] = new Square("Diana", "280 Gold", "yellow", 280, 8, 24, 120, 360, 850, 1025, 1200);
+
+square[30] = new Square("Allez en Prison", "Blitzcrank vous attrape !", "white");
+
+// GROUPE VERT - ZAUN (Warwick, Singed, Urgot)
+square[31] = new Square("Warwick", "300 Gold", "green", 300, 9, 26, 130, 390, 900, 1100, 1275);
+square[32] = new Square("Singed", "300 Gold", "green", 300, 9, 26, 130, 390, 900, 1100, 1275);
+square[33] = new Square("Coffre Hextech", "BUTIN EN COURS D'OUVERTURE...", "white");
+square[34] = new Square("Urgot", "320 Gold", "green", 320, 9, 28, 150, 450, 1000, 1200, 1400);
+
+// GARE 4
+square[35] = new Square("Dragon de Vent", "200 Gold", "white", 200, 1);
+
+square[36] = new Square("Ping SS", "ENNEMI DISPARU ! SURPRISE...", "white");
+
+// GROUPE BLEU - FRÈRES IONIENS (Yone & Yasuo)
+square[37] = new Square("Yone", "350 Gold", "blue", 350, 10, 35, 175, 500, 1100, 1300, 1500);
+square[38] = new Square("BOUTIQUE", "Payez 75 Gold", "white");
+square[39] = new Square("Yasuo", "400 Gold", "blue", 400, 10, 50, 200, 600, 1400, 1700, 2000);
 
 var communityChestCards = [];
 var chanceCards = [];
+communityChestCards[0] = new Card("Vous achetez un QSS, vous échappez à tout contrôle. Sortez de prison gratuitement. Carte à conserver.", function() { p.communityChestJailCard = true; updateOwned();});
+communityChestCards[1] = new Card("Gangplank lance son ultime. Gagnez 10 gold.", function() { addamount(10, 'Community Chest');});
+communityChestCards[2] = new Card("Vos runes First Strike s’activent parfaitement. Gagnez 45 gold.", function() { addamount(45, 'Community Chest');});
+communityChestCards[3] = new Card("Vous participez à une élimination. Assist sur un kill : gagnez 100 gold.", function() { addamount(100, 'Community Chest');});
+communityChestCards[4] = new Card("Votre rune Cashback vous récompense. Gagnez 20 gold.", function() { addamount(20, 'Community Chest');});
+communityChestCards[5] = new Card("Pyke exécute un ennemi avec son ultime. Partage du butin : gagnez 100 gold.", function() { addamount(100, 'Community Chest');});
+communityChestCards[6] = new Card("Vous dominez complètement votre lane. Mid gap : gagnez 100 gold.", function() { addamount(100, 'Community Chest');});
+communityChestCards[7] = new Card("Une pluie d’or tombe sur la Faille. Gagnez 25 gold.", function() { addamount(25, 'Community Chest');});
+communityChestCards[8] = new Card("Vous achetez une balise de contrôle (pink ward). Payez 100 gold.", function() { subtractamount(100, 'Community Chest');});
+communityChestCards[9] = new Card("Vous récupérez un shutdown important. Gagnez 200 gold.", function() { addamount(200, 'Community Chest');});
+communityChestCards[10] = new Card("Vous achetez un élixir de rage. Payez 150 gold.", function() { subtractamount(150, 'Community Chest');});
+communityChestCards[11] = new Card("Vous achetez une potion pour survivre en lane. Payez 50 gold.", function() { subtractamount(50, 'Community Chest');});
+communityChestCards[12] = new Card("Vous recevez 9 honneurs après la partie. Chaque joueur vous donne 50 gold.", function() { collectfromeachplayer(50, 'Community Chest');});
+communityChestCards[13] = new Card("Vous lancez un recall. Retournez à la case départ (Collectez 200 gold).", function() { advance(0);});
+communityChestCards[14] = new Card("Vos structures sont en ruine après un siège. Payez 40 gold par inhibiteur et 115 gold par nexus.", function() { streetrepairs(40, 115);});
+communityChestCards[15] = new Card("Blitzcrank vous attrape avec son Q. Allez directement en prison. Ne passez pas par la case départ.", function() { gotojail();});
 
-communityChestCards[0] = new Card("Get out of Jail, Free. This card may be kept until needed or sold.", function() { p.communityChestJailCard = true; updateOwned();});
-communityChestCards[1] = new Card("You have won lifetime home delivery of the New York Times. Collect $10", function() { addamount(10, 'Community Chest');});
-communityChestCards[2] = new Card("From sale of Macy's stock, you get $45", function() { addamount(45, 'Community Chest');});
-communityChestCards[3] = new Card("Life insurance matures. Collect $100", function() { addamount(100, 'Community Chest');});
-communityChestCards[4] = new Card("Deloitte & Touche LLP tax return Collect $20", function() { addamount(20, 'Community Chest');});
-communityChestCards[5] = new Card("FAO Schwarz Xmas fund matures. Collect $100", function() { addamount(100, 'Community Chest');});
-communityChestCards[6] = new Card("You have won a United Airlines trip around the world! Collect $100", function() { addamount(100, 'Community Chest');});
-communityChestCards[7] = new Card("Performed a wedding at the Plaza Hotel. Receive $25", function() { addamount(25, 'Community Chest');});
-communityChestCards[8] = new Card("Pay hospital $100", function() { subtractamount(100, 'Community Chest');});
-communityChestCards[9] = new Card("You won the Lottery! Collect $200", function() { addamount(200, 'Community Chest');});
-communityChestCards[10] = new Card("Pay school tax of $150", function() { subtractamount(150, 'Community Chest');});
-communityChestCards[11] = new Card("Doctor's fee. Pay $50", function() { subtractamount(50, 'Community Chest');});
-communityChestCards[12] = new Card("Madison Square Garden opening tonight. Collect $50 from every player for opening night seats.", function() { collectfromeachplayer(50, 'Community Chest');});
-communityChestCards[13] = new Card("You have won kiss cash! Advance to GO (Collect $200)", function() { advance(0);});
-communityChestCards[14] = new Card("You are assessed for street repairs. $40 per house. $115 per hotel.", function() { streetrepairs(40, 115);});
-communityChestCards[15] = new Card("Go to Jail. Go directly to Jail. Do not pass GO. Do not collect $200.", function() { gotojail();});
 
-
-chanceCards[0] = new Card("Get out of Jail free. This card may be kept until needed or sold.", function() { p.chanceJailCard=true; updateOwned();});
-chanceCards[1] = new Card("Make general repairs on all your property. For each house pay $25. For each hotel $100.", function() { streetrepairs(25, 100);});
-chanceCards[2] = new Card("Pay poor tax of $15.", function() { subtractamount(15, 'Chance');});
-chanceCards[3] = new Card("You have been elected chairman of Con Edison. Pay each player $50.", function() { payeachplayer(50, 'Chance');});
-chanceCards[4] = new Card("Go back 3 spaces.", function() { gobackthreespaces();});
-chanceCards[5] = new Card("Advance token to the nearest Con Edison utility. If UNOWNED you may buy it from the bank. If OWNED, throw dice and pay owner a total of ten times the amount thrown.", function() { advanceToNearestUtility();});
-chanceCards[6] = new Card("Citibank pays you interest of $50.", function() { addamount(50, 'Chance');});
-chanceCards[7] = new Card("Advance token to the nearest Transportation and pay owner Twice the Rental to which they are otherwise entitled. If Transportation is unowned, you may buy it from the Bank.", function() { advanceToNearestRailroad();});
-chanceCards[8] = new Card("Take a walk past The Essex House. Advance to GO. Collect $200.", function() { advance(0,32);});
-chanceCards[9] = new Card("Take a ride to the Regency Hotel! If you pass GO collect $200.", function() { advance(31);});
-chanceCards[10] = new Card("Take a walk on fifth avenue. Advance token to Trump Tower.", function() { advance(39);});
-chanceCards[11] = new Card("Advance to thirteen.", function() { advance(13);});
-chanceCards[12] = new Card("Your Smith Barney mutual fund pays dividend. Collect $150.", function() { addamount(150, 'Chance');});
-chanceCards[13] = new Card("Advance token to the nearest Transportation and pay owner Twice the Rental to which they are otherwise entitled.\n\nIf Transportation is unowned, you may buy it from the Bank.", function() { advanceToNearestRailroad();});
-chanceCards[14] = new Card("Catch a bus to Central Park. If you pass GO, collect $200.", function() { advance(9);});
-chanceCards[15] = new Card("Go directly to Jail. Do not pass GO, do not collect $200.", function() { gotojail();});
+chanceCards[0] = new Card("Vous achetez un QSS, vous échappez à tout contrôle. Sortez de prison gratuitement. Carte à conserver.", function() { p.chanceJailCard=true; updateOwned();});
+chanceCards[1] = new Card("Vos structures sont endommagées après un siège. Payez 25 gold par inhibiteur et 100 gold par nexus.", function() { streetrepairs(25, 100);});
+chanceCards[2] = new Card("Vous achetez une épée longue pour renforcer votre équipement. Payez 15 gold.", function() { subtractamount(15, 'Chance');});
+chanceCards[3] = new Card("Vous recevez 4 honneurs après la partie. Donnez 50 gold à chaque joueur.", function() { payeachplayer(50, 'Chance');});
+chanceCards[4] = new Card("Ekko remonte le temps avec son ultime. Reculez de 3 cases.", function() { gobackthreespaces();});
+chanceCards[5] = new Card("Vous utilisez une plante de soin. Avancez jusqu’au prochain point de heal. S’il est possédé, payez le double.", function() { advanceToNearestUtility();});
+chanceCards[6] = new Card("Twisted Fate vous fait gagner au jeu. Recevez 20 gold.", function() { addamount(20, 'Chance');});
+chanceCards[7] = new Card("Vous utilisez un téléport vers le dragon. Avancez jusqu’à la prochaine gare. Si elle est possédée, payez le double.", function() { advanceToNearestRailroad();});
+chanceCards[8] = new Card("Vous découvrez un artefact ancien. Avancez jusqu’à la case Ornn.", function() { advance(9); });
+chanceCards[9] = new Card("Vous allez voir le général. Avancez directement à la case Swain.", function() { advance(31);});
+chanceCards[10] = new Card("Vous partez en duel contre Yasuo. Avancez directement à la case Yasuo.", function() { advance(39);});
+chanceCards[11] = new Card("Shen vous protège avec son ultime. Avancez directement à la case Shen.", function() { advance(13);});
+chanceCards[12] = new Card("Vous réalisez un solo kill. Gagnez 150 gold.", function() { addamount(150, 'Chance');});
+chanceCards[13] = new Card("Vous utilisez un téléport vers le dragon. Avancez jusqu’à la prochaine gare. Si elle est possédée, payez le double.", function() { advanceToNearestRailroad();});
+chanceCards[14] = new Card("Vous vous dirigez vers le dragon des océans. Avancez jusqu’à cette case.", function() { advance(5); });
+chanceCards[15] = new Card("Nautilus vous attrape avec son Q. Allez directement en prison. Ne passez pas par la case départ.", function() { gotojail();});
